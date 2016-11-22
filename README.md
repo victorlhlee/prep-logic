596c19b43878f3e1f2147dc1001427a24a3d464d
kwhitejr <kwhitejr.edu@gmail.com>
2016-11-21 20:20:49 -1000

    revised exercises and readme

--- README.md
+++ README.md
@@ -1,1 +1,129 @@
 # prep-logic
+
+# 1) `Function - youGetTaco`
+```javascript
+@param Datatype: String action
+@return Datatype: String
+```
+
+Write a function that takes a single String parameter named `action`. If the value passed into the function is "eat", then the function will return a String with the value "EAT TACOS."
+
+# 2) `Function - isNumberGreaterThan`
+```javascript
+@param Datatype: Number first
+@param Datatype: Number second
+@return Datatype: Boolean
+```
+
+Write a function that takes two parameters of type Number named `first` and `second` respectively. Return true if the first number is greater than the second.
+
+#3) `Function - isTrue`
+```javascript
+@param Datatype: Boolean val
+@return Datatype: Boolean
+```
+
+Write a function that takes in a Boolean value named `val` and have the function return whether the value is true.
+
+#4) `Function - isFalse`
+```javascript  
+@param Datatype: Boolean val
+@return Datatype: Boolean
+```
+
+Write a function that takes in a Boolean value named `val` and have the function return whether the value is false.
+
+#5) `Function - isEqual`
+```javascript
+@param Datatype: String firstWord
+@param Datatype: String secondWord
+@return Datatype: String
+```
+
+Write a function that takes two variables of type String named `firstWord` and `secondWord` respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'. 
+
+#6) `Function - isNotEqual`
+```javascript
+@param Datatype: String firstWord
+@param Datatype: String secondWord
+@return Datatype: String
+```
+
+Write a function that takes two variables of type String named `firstWord` and `secondWord` respectively. Return 'AWWWWRIGHT' if the two are **not** equal otherwise return 'Y U MATCH!'.
+
+#7) `Function - doubleEquals`
+```javascript
+@param Datatype: Boolean first
+@param Datatype: Boolean second
+@return Datatype: Boolean
+```
+
+Write a function that takes two variables of type Boolean named `first` and `second` respectively. Return true if both values are true otherwise return false.
+
+#8) `Function - totalOver30`
+```javascript
+@param Datatype: Number first
+@param Datatype: Number second
+@param Datatype: Number third
+@return Datatype: Boolean
+```
+
+Write a function that takes three variables of type Number named `first`, `second`, and third. Return true if the sum of all values are greater than 30; otherwise, return false.
+
+#9) `Function - totalUnderWhat`
+```javascript
+@param Datatype: Number first
+@param Datatype: Number second
+@param Datatype: Number third
+@param Datatype: Number fourth
+@return Datatype: Boolean
+```
+
+Write a function that takes four variables of type Number named `first`, `second`, `third`, and `fourth`. Return true if the sum of `first`, `second` and `third` are less than `fourth`; otherwise, return false.
+
+#10) `Function - looptoNumber`
+```javascript
+@param Datatype: Number limit
+```
+
+Write a function that takes a single argument, a Number value named `limit`, and have the function write a for-loop that iterates a number of times equal to `limit` and logs the current value of `limit` on each iteration.
+
+#11) `Function - showEachValue`
+```javascript
+@param Datatype: Array characters
+```
+
+Write a function that takes in a Array value named `characters`, which is a sequence of single character String values. The function should execute a for-loop that iterates over the array and prints each character. 
+
+#12) `Function - createArrayFromString`
+```javacript
+@param Datatype: String word
+@return Datatype: Array
+```
+
+Write a function that takes a single variable of type String named `word` and write a for loop that creates an Array made up of each character in `word` except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function.
+
+#13) `Function - greatSummator`
+```javascript
+@param Datatype: Array
+@return Datatype: Number
+```
+
+Write a function that takes an Array with sequence of Number values. The function should return the sum of the Array's Number values.
+
+#14) `Function - totalUnderWhatFor`
+```javascript
+@param Datatype: Array 
+@param Datatype: Number total 
+@return Datatype: Boolean
+```
+
+Write a function that takes two arguments: (1) an Array with sequence of Number values, and (2) a Number named `total`. Return true if the sum of all values in the Array are less than `total`; otherwise, return false.
+
+#15) `Function - checkTrueValues`
+```javascript
+@param Datatype: Array 
+@return Datatype: Boolean
+```
+
+Write a function that takes an Array containing any number of type Boolean values. The function should execute a for-loop that invokes the `isTrue` function (defined earlier) with each value from the Array as input. The `checkTrueValues` function should return true if all values from the `isTrue` function return true.
--- functions.js
+++ functions.js
@@ -2,12 +2,13 @@
 If statments are a way for our applications to take different paths through our code or make choices about what kind of behavior our application takes based on certain conditions or values in our variables. The variable or value that we are testing to be true is known as the condition.*/
 /*In Human: If this condition is true, do something.*/
 
-var isTurnedOn = false;
+var isTurnedOn = true;
 function flipSwitch(){
   if(isTurnedOn){
     isTurnedOn = false;
   }
 }
+
 /*If/Else Statements
 If/Else statments have the same behavior as our if statement above but we can add a secondary condition in the case that our if statement is not true to take another action instead. What if our light is already turned off when we flip the switch?
 
@@ -22,6 +23,7 @@ function flipSwitch(){
     isTurnedOn = true;
   }
 }
+
 /*For loops
 Sometimes we want to loop over a collection of data stored in an Arrayor inspect each character in a String. Or we just want to execute a block of code a set number of times before continuing on with the execution of our program. For loops allow us to execute a block of code and at the end of each iteration of our for loop we will evaluate a condition to determine if our loop should run again similar to our if statment.
 
@@ -42,81 +44,146 @@ function makeCrazyLongString(string){
   }
 }
 
+/* 
+1) Function - youGetTaco
 
-/*makeCrazyLongString('taco');
-Function - youGetTaco
-Write a function that takes a single String parameter called action. If the value passed into our function is eat have the function return the String value EAT TACOS.
+  @param Datatype: String `action`
+  @return Datatype: String
+
+Write a function that takes a single String parameter named `action`. If the value passed into the function is "eat", then the function will return a String with the value "EAT TACOS."
 */
 
+// Step 1: define the function
+
+// Step 2: invoke the function and store result to a variable
+
+// Step 3: test your code with a console log
 
 
-/*Function - isNumberGreaterThan
+/*
+2) Function - isNumberGreaterThan
+
+  @param Datatype: Number `first`
+  @param Datatype: Number `second`
+  @return Datatype: Boolean
+
 Write a function that takes two parameters of type Number called first and second respectively. Return true if the first number is greater than the second.*/
 
 
+/* 3) Function - isTrue
 
-/*Function - isTrue
+  @param Datatype: Boolean `val`
+  @return Datatype: Boolean
+
 Write a function that takes in a Boolean value named val and have the function return whether the value is true.*/
 
 
+/* 4) Function - isFalse
+  
+  @param Datatype: Boolean `val`
+  @return Datatype: Boolean
+
+Write a function that takes in a Boolean value named val and have the function return whether the value is false. */
 
 
-/*Function - isFalse
-Write a function that takes in a Boolean value named val and have the function return whether the value is false.*/
+/* 5) Function - isEqual
+
+  @param Datatype: String `firstWord`
+  @param Datatype: String `secondWord`
+  @return Datatype: String
+
+Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'. */
+
+ 
+/* 6) Function - isNotEqual
+
+  @param Datatype: String `firstWord`
+  @param Datatype: String `secondWord`
+  @return Datatype: String
+
+Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are **not** equal otherwise return 'Y U MATCH!'. */
 
 
+/* 7) Function - doubleEquals
+
+  @param Datatype: Boolean `first`
+  @param Datatype: Boolean `second`
+  @return Datatype: Boolean
+
+Write a function that takes two variables of type Boolean called first and second respectively. Return true if both values are true otherwise return false. */
 
 
-/*Function - isEqual
-Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'.*/
+/* 8) Function - totalOver30
 
+  @param Datatype: Number `first`
+  @param Datatype: Number `second`
+  @param Datatype: Number `third`
+  @return Datatype: Boolean
 
-/*Function - isNotEqual
-Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are not equal otherwise return 'Y U MATCH!'.*/
-
-
-/*Function - doubleEquals
-Write a function that takes two variables of type Boolean called first and second respectively. Return true if both values are true otherwise return false.*/
-
-
-
-/*Function - totalOver30
-Write a function that takes three variables of type Number called first and second and third respectively. Return true if the sum of all values are greater than 30 otherwise return false.*/
-
-
-
-/*Function - totalUnderWhat
-Write a function that takes four variables of type Number called first and second and third and fourth respectively. Return true if the sum of first, second and third are less than fourth otherwise return false.*/
-
-
-
-/*Function - looptoNumber
-Write a function that takes in a Number value named limit and have the function write a for loop that loops the number of times of limit and console.log each number as the loop executes.*/
-
-
-
-/*Function - showEachValue
-Write a function that takes in a Array value named characters that has a sequence of single character String values and have the function write a for loop that loops for each number of elements in the Array and console.log each character as the loop executes.*/
-
-
-
-/*Function - createArrayFromString
-Write a function that takes a single variable of type String called word and write a for loop that creates an Array made up of each character in word except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function.*/
-
-
-
-/*Function - greatSummator
-Write a function that takes an Array with any number of type Number and write a for loop to add all numbers in the Array and return the sum.*/
+Write a function that takes three variables of type Number called first and second and third respectively. Return true if the sum of all values are greater than 30 otherwise return false. */
 
 
 
+/* 9) Function - totalUnderWhat
 
-/*Function - totalUnderWhatFor
-Write a function that takes an Array with any number of type Number and second variable called total. Return true if the sum of all values in the Array are less than total otherwise return false.*/
+  @param Datatype: Number `first`
+  @param Datatype: Number `second`
+  @param Datatype: Number `third`
+  @param Datatype: Number `fourth`
+  @return Datatype: Boolean
+
+Write a function that takes four variables of type Number called `first` and `second` and `third` and `fourth` respectively. Return true if the sum of `first`, `second` and `third` are less than `fourth`; otherwise, return false. */
 
 
 
+/* 10) Function - looptoNumber
 
-/*Function - checkTrueValues
-Write a function that takes an Array with any number of type Boolean values and write a for loop to call our isTrue function with each value as input and return true if all values return true from our isTrue function.*/
+  @param Datatype: Number `limit`
+
+Write a function that takes a single argument, a Number value named `limit`, and have the function write a for-loop that iterates a number of times equal to `limit` and logs the current value of `limit` on each iteration. */
+
+
+
+/* 11) Function - showEachValue
+
+  @param Datatype: Array `characters`
+
+Write a function that takes in a Array value named `characters`, which is a sequence of single character String values. The function should execute a for-loop that iterates over the array and prints each character. */
+
+
+
+/* 12) Function - createArrayFromString
+
+  @param Datatype: String `word`
+  @return Datatype: Array
+
+Write a function that takes a single variable of type String called `word` and write a for loop that creates an Array made up of each character in `word` except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function. */
+
+
+
+/* 13) Function - greatSummator
+
+  @param Datatype: Array 
+  @return Datatype: Number
+
+Write a function that takes an Array with sequence of Number values. The function should return the sum of the Array's Number values. */
+
+
+
+/* 14) Function - totalUnderWhatFor
+
+  @param Datatype: Array 
+  @param Datatype: Number `total` 
+  @return Datatype: Boolean
+
+Write a function that takes two arguments: (1) an Array with sequence of Number values, and (2) a Number named `total`. Return true if the sum of all values in the Array are less than `total`; otherwise, return false. */
+
+
+
+/* 15) Function - checkTrueValues
+
+  @param Datatype: Array 
+  @return Datatype: Boolean
+
+Write a function that takes an Array containing any number of type Boolean values. The function should execute a for-loop that invokes the `isTrue` function (defined earlier) with each value from the Array as input. The `checkTrueValues` function should return true if all values from the `isTrue` function return true. */
 
